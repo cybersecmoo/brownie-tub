0aa698dd7f97ae1d505927ffbc6210bc27f5a630
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2020-01-26 13:27:44 +0000

    Lay Some Foundations for Tests

---
+++ test/spectron-helper.js
@@ -0,0 +1,26 @@
+import { Application } from "spectron";
+
+class TestHelper {
+	initialiseSpectron = () =>{
+		var electronPath = path.join(__dirname, "../node_modules", ".bin", "electron");
+		const appPath = path.join(__dirname, "../dist");
+
+		if(process.platform === "win32") {
+			electronPath += ".cmd";
+		}
+
+		return new Application({
+			path: electronPath,
+			args: [appPath],
+			env: {
+				ELECTRON_ENABLE_LOGGING: true,
+				ELECTRON_ENABLE_STACK_DUMPING: true,
+				NODE_ENV: "development"
+			},
+			startTimeout: 20000,
+			chromeDriverLogPath: "../chromedriverlog.txt"
+		});
+	}
+}
+
+module.exports = TestHelper;
