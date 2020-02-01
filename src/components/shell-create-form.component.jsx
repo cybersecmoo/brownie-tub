a62e45c22eef4d57038b25fb17ba5c9204893615
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2020-02-01 17:39:16 +0000

    Finish Form Layout

--- src/components/shell-create-form.component.jsx
+++ src/components/shell-create-form.component.jsx
@@ -9,6 +9,8 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import { makeStyles } from '@material-ui/core/styles';
 import "./shell-list.css";
 
@@ -23,42 +25,92 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const ShellCreateForm = (props) => {
-  const classes = useStyles();
-	const [paramType, setParamType] = React.useState("");
+	const classes = useStyles();
+	const [state, setState] = React.useState({
+		ipOrHostname: "",
+		paramType: "",
+		commandParam: "",
+		passwordEnabled: false,
+		passwordParam: "",
+		password: "",
+		os: "",
+		isAdmin: false
+	});
 
 	const handleSubmit = () => {
-		console.log("Yay");
+		console.log(state);
+		props.onClose();
 	}
 
-	const handleChange = event => {
-		setParamType(event.target.value);
-	};
+	const handleStringChanged = name => event => {
+		setState({ ...state, [name]: event.target.value });
+	}
+
+	const handleCheckboxChanged = name => event => {
+		setState({ ...state, [name]: event.target.checked });
+	}
 
 	return (
 		<Dialog open={props.open} onClose={props.onClose}>
 			<DialogContent>
 				<DialogTitle>Register a New Shell</DialogTitle>
-				<FormControl className={classes.formControl}>
-					<TextField autoFocus id="ipOrHostname" label="IP or URL" fullWidth />
-				</FormControl>
+				<div>
+					<FormControl className={classes.formControl}>
+						<TextField autoFocus id="ipOrHostname" label="IP or URL" onChange={handleStringChanged("ipOrHostname")} fullWidth />
+					</FormControl>
+				</div>
 				
-				<FormControl className={classes.formControl}>
-					<InputLabel id="commandParamType-label">Param Type</InputLabel>
-					<Select
-						labelId="commandParamType-label"
-						id="commandParamType"
-						value={paramType}
-						onChange={handleChange}
-					>
-						<MenuItem value="">
-							<em>None</em>
-						</MenuItem>
-						<MenuItem value={"header"}>Header</MenuItem>
-						<MenuItem value={"cookie"}>Cookie</MenuItem>
-						<MenuItem value={"POST"}>POST Param</MenuItem>
-						<MenuItem value={"GET"}>GET Param</MenuItem>
-					</Select>
-				</FormControl>
+				<div>
+					<FormControl className={classes.formControl}>
+						<InputLabel id="commandParamType-label">Param Type</InputLabel>
+						<Select
+							labelId="commandParamType-label"
+							id="commandParamType"
+							value={state.paramType}
+							onChange={handleStringChanged("paramType")}
+						>
+							<MenuItem value="">
+								<em>None</em>
+							</MenuItem>
+							<MenuItem value={"header"}>Header</MenuItem>
+							<MenuItem value={"cookie"}>Cookie</MenuItem>
+							<MenuItem value={"POST"}>POST Param</MenuItem>
+							<MenuItem value={"GET"}>GET Param</MenuItem>
+						</Select>
+					</FormControl>
+					<FormControl className={classes.formControl}>
+						<TextField id="commandParam" label="Command Parameter" fullWidth />
+					</FormControl>
+				</div>
+
+				<div>
+					<FormControlLabel control={
+						<Checkbox checked={state.passwordEnabled} onChange={handleCheckboxChanged("passwordEnabled")} value="passwordEnabled" />
+					} 
+					label="Enable Password" />
+				</div>
+	
+				<div>
+					<FormControl className={classes.formControl}>
+						<TextField id="passwordParam" label="Password Parameter" onChange={handleStringChanged("passwordParam")} fullWidth />
+					</FormControl>
+					<FormControl className={classes.formControl}>
+						<TextField id="password" label="Password" onChange={handleStringChanged("password")} fullWidth />
+					</FormControl>
+				</div>
+
+				<div>
+					<FormControl className={classes.formControl}>
+						<TextField id="os" label="Operating System" onChange={handleStringChanged("os")} fullWidth />
+					</FormControl>
+				</div>
+
+				<div>
+					<FormControlLabel control={
+						<Checkbox checked={state.isAdmin} onChange={handleCheckboxChanged("isAdmin")} value="isAdmin" />
+					} 
+					label="Is Admin" />
+				</div>
 			</DialogContent>
 			<DialogActions>
 				<Button onClick={props.onClose} variant="contained" color="secondary">Cancel</Button>
