0c556916bced9a947c3a58bdd75bcd2aad2ab7c9
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2020-02-01 11:52:00 +0000

    Split out Form into Separate Component

--- src/components/shell-list.component.jsx
+++ src/components/shell-list.component.jsx
@@ -5,33 +5,11 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ComputerIcon from '@material-ui/icons/Computer';
 import AddIcon from '@material-ui/icons/Add';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Button from '@material-ui/core/Button';
-import { DialogContent } from '@material-ui/core';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
-import { makeStyles } from '@material-ui/core/styles';
-import FormControl from '@material-ui/core/FormControl';
-import "./shell-list.scss";
-
-const useStyles = makeStyles(theme => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-}));
+import ShellCreateForm from "./shell-create-form.component";
+import "./shell-list.css";
 
 const ShellList = () => {
-  const classes = useStyles();
 	const [open, setOpen] = React.useState(false);
-	const [paramType, setParamType] = React.useState("");
 
 	const handleFormOpen = () => {
 		setOpen(true);
@@ -39,14 +17,6 @@ const ShellList = () => {
 
 	const handleFormClose = () => {
 		setOpen(false);
-	};
-
-	const handleSubmit = () => {
-		console.log("Yay");
-	}
-
-	const handleChange = event => {
-		setParamType(event.target.value);
 	};
 
 	return (
@@ -65,38 +35,7 @@ const ShellList = () => {
 					<ListItemText primary="Add New" />
 				</ListItem>
 			</List>
-
-			<Dialog open={open} onClose={handleFormClose}>
-				<DialogContent>
-					<DialogTitle>Register a New Shell</DialogTitle>
-      		<FormControl className={classes.formControl}>
-						<TextField autoFocus id="ipOrHostname" label="IP or URL" fullWidth />
-					</FormControl>
-					
-					<FormControl className={classes.formControl}>
-						<InputLabel id="commandParamType-label">Param Type</InputLabel>
-						<Select
-							labelId="commandParamType-label"
-							id="commandParamType"
-							value={paramType}
-							onChange={handleChange}
-						>
-							<MenuItem value="">
-								<em>None</em>
-							</MenuItem>
-							<MenuItem value={"header"}>Header</MenuItem>
-							<MenuItem value={"cookie"}>Cookie</MenuItem>
-							<MenuItem value={"POST"}>POST Param</MenuItem>
-							<MenuItem value={"GET"}>GET Param</MenuItem>
-						</Select>
-					</FormControl>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleFormClose} variant="contained" color="secondary">Cancel</Button>
-					<Button onClick={handleSubmit} variant="contained" color="primary">Submit</Button>
-				</DialogActions>		
-			</Dialog>
-			
+			<ShellCreateForm open={open} onClose={handleFormClose} />					
 		</div>
 		
 	);
