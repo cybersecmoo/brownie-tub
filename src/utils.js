6c035c83052b54f8d7aa8a0af9979c4f91ddeaaf
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2020-02-07 21:42:23 +0000

    Set up Axios Config for all Param Types

--- src/utils.js
+++ src/utils.js
@@ -1,33 +1,69 @@
 import axios from "axios";
 import { WINDOWS } from "./osTypes";
 
+const generateConfig = (shell, command) => {
+	var config = {
+		headers: {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
+		}
+	};
+
+	switch(shell.commandParamType) {
+		case "header":
+			config["headers"][shell.commandParam] = command;
+
+			if(shell.passwordEnabled) {
+				config["headers"][shell.passwordParam] = shell.password;
+			}
+
+			break;
+		case "cookie":
+			config["headers"]["Cookie"] = `${shell.commandParam}=${command}`;
+
+			if(shell.passwordEnabled) {
+				config["headers"]["Cookie"] = `${shell.passwordParam}=${shell.password}`;
+			}
+
+			break;
+		case "POST":
+			config["data"][shell.commandParam] = command;			
+
+			if(shell.passwordEnabled) {
+				config["data"][shell.passwordParam] = shell.password;
+			}
+
+			break;
+		case "GET":
+			config["params"][shell.commandParam] = command;			
+
+			if(shell.passwordEnabled) {
+				config["params"][shell.passwordParam] = shell.password;
+			}
+
+			break;
+	}
+	
+	console.log(config);
+	return config;
+}
+
 export const listDirectoryRequest = async (shell) => {
-	// TODO Set the password parameter, if required
-	// TODO send the request and return the response
 	var command = "ls -la";
 
 	if(shell.os === WINDOWS) {
 		command = "dir";
 	}
 
-	switch(shell.commandParamType) {
-		case "header":
-			const config = {
-				headers: {
-					"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
-				}
-			};
-			config["headers"][shell.commandParam] = command;
-			await axios.get(shell.ipOrHostname, config);
-			break;
-		case "cookie":
+	const config = generateConfig(shell, command);
+	var response;
 
-			break;
-		case "POST":
-			break;
-		case "GET":
-			break;
+	if(shell.commandParamType === "POST") {
+		response = await axios.post(shell.ipOrHostname, config);
+	} else {
+		response = await axios.get(shell.ipOrHostname, config);
 	}
+
+	return response;
 }
 
 // TODO Make a fn to parse list-dir responses (using operating system info to determine how to work out which ones are dirs and which are files)
