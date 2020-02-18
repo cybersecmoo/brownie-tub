cfa2700a2acaf7681fe558580cd5c9f5bf2f31ad
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2020-02-18 02:31:15 +0000

    Add Return to Dir View

--- src/components/directory-view.component.jsx
+++ src/components/directory-view.component.jsx
@@ -18,28 +18,31 @@ class DirectoryView extends Component {
 	}
 
 	render() {
-		<List aria-label="directory">
-			{
-				this.state.dir.map((entry, index) => {
-					if(entry.type == "DIR") {
-						return (
-							<ListItem button>
-								<FolderIcon />
-								<ListItemText primary={entry.name} />
-							</ListItem>
-						);
-					} else {
-						return (
-							<ListItem button>
-								<FileIcon />
-								<ListItemText primary={entry.name} />
-							</ListItem>
-						);
-					}
-					
-				})
-			}
-		</List>
+		return (
+			<List aria-label="directory">
+				{
+					this.state.dir.map((entry, index) => {
+						if(entry.type == "DIR") {
+							return (
+								<ListItem button>
+									<FolderIcon />
+									<ListItemText primary={entry.name} />
+								</ListItem>
+							);
+						} else {
+							return (
+								<ListItem button>
+									<FileIcon />
+									<ListItemText primary={entry.name} />
+								</ListItem>
+							);
+						}
+						
+					})
+				}
+			</List>
+		);
+		
 	}
 }
 
