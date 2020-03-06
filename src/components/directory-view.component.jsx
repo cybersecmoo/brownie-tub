d4306dd43efa787b0eb19abaeca684dd13d81503
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2020-03-06 19:41:03 +0000

    Add Context Menu

--- src/components/directory-view.component.jsx
+++ src/components/directory-view.component.jsx
@@ -1,78 +1,91 @@
 import React, { Component } from "react";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import FolderIcon from '@material-ui/icons/Folder';
-import FileIcon from '@material-ui/icons/InsertDriveFile';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import FolderIcon from "@material-ui/icons/Folder";
+import FileIcon from "@material-ui/icons/InsertDriveFile";
 import "./dirview.css";
 import { Typography } from "@material-ui/core";
+import ContextMenu from "react-context-menu";
 
 class DirectoryView extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			dirName: "Directory",
-			dir: [
-				{
-					type: "DIR",
-					name: "exampleDir"
-				},
-				{
-					type: "FILE",
-					name: "exampleFile"
-				}
-			]
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      dirName: "Directory",
+      dir: [
+        {
+          type: "DIR",
+          name: "exampleDir"
+        },
+        {
+          type: "FILE",
+          name: "exampleFile"
+        }
+      ]
+    };
 
-		window.ipcRenderer.on("shell:select-reply", (event, response) => {
-			this.setState({dirName: response.dirName, dir: response.dir});
-		});
+    window.ipcRenderer.on("shell:select-reply", (event, response) => {
+      this.setState({ dirName: response.dirName, dir: response.dir });
+    });
 
-		this.handleDirClick = this.handleDirClick.bind(this);
-	}
+    this.handleDirClick = this.handleDirClick.bind(this);
+    this.handleCreateFile = this.handleCreateFile.bind(this);
+    this.handleCreateDir = this.handleCreateDir.bind(this);
+  }
 
-	handleDirClick = (file) => (event) => {
-		event.preventDefault();
+  handleDirClick = file => event => {
+    event.preventDefault();
 
-		if(file.type === "DIR") {
-			window.ipcRenderer.send("file:change-directory", {dir: file.name, pwd: this.state.dirName});
-		} else {
+    if (file.type === "DIR") {
+      window.ipcRenderer.send("file:change-directory", { dir: file.name, pwd: this.state.dirName });
+    } else {
+			// TODO Implement the electron side of this
+			// TODO Implement the handler for the response (show a modal panel with the fie contents)
+      window.ipcRenderer.send("file:view-file", { file: file.name, pwd: this.state.dirName });
+    }
+  };
 
-		}
-		// TODO For files: show some form of modal panel? Or context menu of some sort
-	}
+	// TODO Implement this
+  handleCreateFile = () => {};
 
-	render() {
-		return (
-			<div>
-				<Typography variant="h5">{this.state.dirName}</Typography>
-				<List className="dir-view" aria-label="directory">
-					{
-						this.state.dir.map((entry, index) => {
-							if(entry.type === "DIR") {
-								return (
-									<ListItem button>
-										<FolderIcon />
-										<ListItemText primary={entry.name} onClick={this.handleDirClick(entry)} />
-									</ListItem>
-								);
-							} else {
-								return (
-									<ListItem button>
-										<FileIcon />
-										<ListItemText primary={entry.name} />
-									</ListItem>
-								);
-							}
-							
-						})
-					}
-				</List>
-			</div>
-			
-		);
-		
-	}
+	// TODO Implement this
+  handleCreateDir = () => {};
+
+  render() {
+    return (
+      <div>
+        <Typography variant="h5">{this.state.dirName}</Typography>
+        <List
+          className="dir-view"
+          aria-label="directory"
+          contextId={"dir-area"}
+          items={[
+            { label: "Create File", onClick: this.handleCreateFile },
+            { label: "Create Directory", onClick: this.handleCreateDir }
+          ]}
+        >
+          {this.state.dir.map((entry, index) => {
+            if (entry.type === "DIR") {
+              return (
+                <ListItem button>
+                  <FolderIcon />
+                  <ListItemText primary={entry.name} onClick={this.handleDirClick(entry)} />
+                </ListItem>
+              );
+            } else {
+              return (
+                <ListItem button>
+                  <FileIcon />
+                  <ListItemText primary={entry.name} />
+                </ListItem>
+              );
+            }
+          })}
+        </List>
+      </div>
+    );
+  }
 }
 
 export default DirectoryView;
