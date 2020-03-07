6441984d42aaa5d2fd1429e87febd208910c1b91
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2020-03-07 12:11:55 +0000

    Remove Placeholder Terminal Lines

--- src/components/terminal.component.jsx
+++ src/components/terminal.component.jsx
@@ -9,10 +9,7 @@ class Terminal extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			terminalLines: [
-				"hi",
-				"bye"
-			],
+			terminalLines: [],
 			currentCommand: ""
 		};
 
@@ -37,8 +34,9 @@ class Terminal extends Component {
 		event.preventDefault();
 
 		if(this.state.currentCommand !== "clear") {
-			window.ipcRenderer.send("terminal:command", this.state.currentCommand);
+			const command = this.state.currentCommand;
 			this.setState({...this.state, currentCommand: ""});
+			window.ipcRenderer.send("terminal:command", command);
 		} else {
 			this.setState({terminalLines: [], currentCommand: ""});
 		}
